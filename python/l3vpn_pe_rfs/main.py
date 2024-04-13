1c422707ec79d82b0a9576d044456d571426e729
bfinnema <bfinnema@cisco.com>
2024-04-13 14:36:57 +0200

    Added python code for JUNOS variables. Added variables in the JUNOS section of the template

--- python/l3vpn_pe_rfs/main.py
+++ python/l3vpn_pe_rfs/main.py
@@ -2,64 +2,27 @@
 import ncs
 from ncs.application import Service
 
-
-# ------------------------
-# SERVICE CALLBACK EXAMPLE
-# ------------------------
 class ServiceCallbacks(Service):
 
-    # The create() callback is invoked inside NCS FASTMAP and
-    # must always exist.
     @Service.create
     def cb_create(self, tctx, root, service, proplist):
         self.log.info('Service create(service=', service._path, ')')
 
+        def netmask_to_cidr(m_netmask):
+            return(sum([ bin(int(bits)).count("1") for bits in m_netmask.split(".") ]))
+
         vars = ncs.template.Variables()
-        vars.add('DUMMY', '127.0.0.1')
+        vars.add('LOOPBACK_IF_NUMBER', '0')
+        vars.add('LOOPBACK_IP_ADDR', '2.2.2.2')
+        vars.add('IPV4_CIDR', netmask_to_cidr(service.interface.ipv4_mask))
+
         template = ncs.template.Template(service)
         template.apply('l3vpn_pe_rfs-template', vars)
 
-    # The pre_modification() and post_modification() callbacks are optional,
-    # and are invoked outside FASTMAP. pre_modification() is invoked before
-    # create, update, or delete of the service, as indicated by the enum
-    # ncs_service_operation op parameter. Conversely
-    # post_modification() is invoked after create, update, or delete
-    # of the service. These functions can be useful e.g. for
-    # allocations that should be stored and existing also when the
-    # service instance is removed.
-
-    # @Service.pre_modification
-    # def cb_pre_modification(self, tctx, op, kp, root, proplist):
-    #     self.log.info('Service premod(service=', kp, ')')
-
-    # @Service.post_modification
-    # def cb_post_modification(self, tctx, op, kp, root, proplist):
-    #     self.log.info('Service postmod(service=', kp, ')')
-
-
-# ---------------------------------------------
-# COMPONENT THREAD THAT WILL BE STARTED BY NCS.
-# ---------------------------------------------
 class Main(ncs.application.Application):
     def setup(self):
-        # The application class sets up logging for us. It is accessible
-        # through 'self.log' and is a ncs.log.Log instance.
         self.log.info('Main RUNNING')
-
-        # Service callbacks require a registration for a 'service point',
-        # as specified in the corresponding data model.
-        #
         self.register_service('l3vpn_pe_rfs-servicepoint', ServiceCallbacks)
 
-        # If we registered any callback(s) above, the Application class
-        # took care of creating a daemon (related to the service/action point).
-
-        # When this setup method is finished, all registrations are
-        # considered done and the application is 'started'.
-
     def teardown(self):
-        # When the application is finished (which would happen if NCS went
-        # down, packages were reloaded or some error occurred) this teardown
-        # method will be called.
-
         self.log.info('Main FINISHED')
